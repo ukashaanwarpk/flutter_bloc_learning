8766e801483a0cbd6838a5861aa6a2b786259745
ukashaanwarpk <anwarukasha0341@gmail.com>
2025-04-19 19:51:24 +0500

    feat: Refactor CartScreen layout to improve product display and interaction

--- lib/ui/tabs/cart/cart_screen.dart
+++ lib/ui/tabs/cart/cart_screen.dart
@@ -12,130 +12,157 @@ class CartScreen extends StatelessWidget {
           title: const Text('Cart Screen'),
           centerTitle: true,
         ),
-        body: Padding(
-          padding: const EdgeInsets.only(
-            top: 20,
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                width: size.width * 0.30,
-                height: size.height * 0.13,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(14),
-                ),
-                child: Center(
-                  child: Image.network(
-                    'https://images.unsplash.com/photo-1743883325575-783014a39a8b?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-                    width: 70,
-                    height: 70,
-                  ),
-                ),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 20,
               ),
-              SizedBox(
-                width: size.width * 0.45,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Mango Juice',
-                      style: GoogleFonts.poppins(
-                        fontSize: size.width * 0.035,
-                        fontWeight: FontWeight.w500,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    width: size.width * 0.30,
+                    height: size.height * 0.13,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(14),
+                    ),
+                    child: Center(
+                      child: Image.network(
+                        'https://images.unsplash.com/photo-1743883325575-783014a39a8b?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                        width: 70,
+                        height: 70,
                       ),
                     ),
-                    SizedBox(
-                      height: size.height * 0.005,
-                    ),
-                    Text(
-                      "\$${40}",
-                      style: GoogleFonts.poppins(
-                        color: Colors.black.withValues(alpha: 0.8),
-                        fontSize: size.width * 0.035,
-                      ),
-                    ),
-                    SizedBox(
-                      height: size.width * 0.030,
-                    ),
-                    Row(
+                  ),
+                  SizedBox(
+                    width: size.width * 0.45,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        GestureDetector(
-                          onTap: () {},
-                          child: Container(
-                            width: 30,
-                            height: 30,
-                            decoration: BoxDecoration(
-                              border: Border.all(
-                                color: Colors.black26,
-                              ),
-                              borderRadius: BorderRadius.circular(100),
-                            ),
-                            child: const Icon(
-                              Icons.add,
-                              color: Colors.black,
-                              size: 14,
-                            ),
+                        Text(
+                          'Mango Juice',
+                          style: GoogleFonts.poppins(
+                            fontSize: size.width * 0.035,
+                            fontWeight: FontWeight.w500,
                           ),
                         ),
-                        const SizedBox(
-                          width: 13,
+                        SizedBox(
+                          height: size.height * 0.005,
                         ),
                         Text(
-                          '2',
-                          style: GoogleFonts.poppins(),
-                        ),
-                        const SizedBox(
-                          width: 13,
-                        ),
-                        GestureDetector(
-                          onTap: () {},
-                          child: Container(
-                            width: 30,
-                            height: 30,
-                            decoration: BoxDecoration(
-                              border: Border.all(
-                                color: Colors.black26,
-                              ),
-                              borderRadius: BorderRadius.circular(100),
-                            ),
-                            child: const Icon(
-                              Icons.remove,
-                              color: Colors.black,
-                              size: 14,
-                            ),
+                          "\$${40}",
+                          style: GoogleFonts.poppins(
+                            color: Colors.black.withValues(alpha: 0.8),
+                            fontSize: size.width * 0.035,
                           ),
-                        )
+                        ),
+                        SizedBox(
+                          height: size.width * 0.030,
+                        ),
+                        Row(
+                          children: [
+                            GestureDetector(
+                              onTap: () {},
+                              child: Container(
+                                width: 30,
+                                height: 30,
+                                decoration: BoxDecoration(
+                                  border: Border.all(
+                                    color: Colors.black26,
+                                  ),
+                                  borderRadius: BorderRadius.circular(100),
+                                ),
+                                child: const Icon(
+                                  Icons.add,
+                                  color: Colors.black,
+                                  size: 14,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 13,
+                            ),
+                            Text(
+                              '2',
+                              style: GoogleFonts.poppins(),
+                            ),
+                            const SizedBox(
+                              width: 13,
+                            ),
+                            GestureDetector(
+                              onTap: () {},
+                              child: Container(
+                                width: 30,
+                                height: 30,
+                                decoration: BoxDecoration(
+                                  border: Border.all(
+                                    color: Colors.black26,
+                                  ),
+                                  borderRadius: BorderRadius.circular(100),
+                                ),
+                                child: const Icon(
+                                  Icons.remove,
+                                  color: Colors.black,
+                                  size: 14,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
                       ],
                     ),
-                  ],
-                ),
-              ),
-              GestureDetector(
-                onTap: () {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                      backgroundColor: Color.fromARGB(255, 247, 247, 247),
-                      content: Text(
-                        "Item removed!",
-                        style: TextStyle(color: Colors.black),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          backgroundColor: Color.fromARGB(255, 247, 247, 247),
+                          content: Text(
+                            "Item removed!",
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        ),
+                      );
+                    },
+                    child: CircleAvatar(
+                      backgroundColor: Colors.redAccent.withValues(alpha: .07),
+                      radius: 18,
+                      child: const Icon(
+                        Icons.delete,
+                        color: Colors.redAccent,
+                        size: 14,
                       ),
                     ),
-                  );
-                },
-                child: CircleAvatar(
-                  backgroundColor: Colors.redAccent.withValues(alpha: .07),
-                  radius: 18,
-                  child: const Icon(
-                    Icons.delete,
-                    color: Colors.redAccent,
-                    size: 14,
-                  ),
-                ),
-              )
-            ],
-          ),
+                  )
+                ],
+              ),
+            ),
+
+            const Row(
+              children: [
+                Text('sub total'),
+                Text('\$${100}'),
+                
+              ],
+            ),
+             
+             const Row(
+              children: [
+                Text('Shipping'),
+                Text('\$${20}'),
+                
+              ],
+            ),
+            const Row(
+              children: [
+                Text('Total'),
+                Text('\$${200}'),
+                
+              ],
+            ),
+          ],
         ));
   }
 }
